65ca1f46273736b4a042b3d484c9ce23840bb525
人豪 鄭 <cheng71057@gmail.com>
2017-04-07 01:49:45 +0800

    0407

---
+++ eSale/Areas/Emp/Controllers/Default1Controller.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication2.Areas.Emp.Controllers
+{
+    public class Default1Controller : Controller
+    {
+        //
+        // GET: /Emp/Default1/
+
+        public ActionResult Index()
+        {
+            ViewBag.emp = "Hello Emp";
+            return View();
+        }
+
+    }
+}
